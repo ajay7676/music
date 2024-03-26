a9f0419e8b512077ff921fe12f9c900e35ff2376
Ajay Chauhan <ajaychauhanpp44@gmail.com>
2024-03-26 10:34:12 +0530

    designed  sticky scroll reveal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from '@/components/FeaturedCourses'
 import Image from "next/image";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
        <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection />
         <FeaturedCourses />
+        <WhyChooseUs />
        </main>
     </>
   )
